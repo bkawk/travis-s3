b7adc26f0becdee987b6cc366dfaac7f4de37edf
bkawk <email.will.in.china@gmail.com>
2018-12-24 15:20:00 +0800

    drawer toggle aria

--- src/my-app.js
+++ src/my-app.js
@@ -95,7 +95,7 @@ class MyApp extends PolymerElement {
 
           <app-header slot="header" condenses="" reveals="" effects="waterfall">
             <app-toolbar>
-              <paper-icon-button icon="my-icons:menu" drawer-toggle=""></paper-icon-button>
+              <paper-icon-button icon="my-icons:menu" drawer-toggle="" aria-label="drawer toggle"></paper-icon-button>
               <div main-title="">My App</div>
             </app-toolbar>
           </app-header>
